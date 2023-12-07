d6d66e15d75ffe9bcf80d3f2864039a5bb620fb6
Mahdi Abd El-Mageed <mahdi.code98@gmail.com>
2023-12-07 00:29:29 -0800

    Initial Commit

---
+++ MyFirstAppTest-IOS/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MyFirstAppTest-IOS
+//
+//  Created by Mahdi Abd El-Mageed on 06/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+     print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+           print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print("viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        print("viewDidDisappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        print("viewWillDisappear")
+    }
+
+
+}
+
